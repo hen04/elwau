ca194c6ca5a0646508e5ab2a7335d73ef693946a
degtyareva <degtyareva@ttcsoft.ru>
2015-08-17 18:34:07 +0300

    second day

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp'),
     stylus = require('gulp-stylus'), 
     jade = require('gulp-jade'), 
+    autoprefixer = require('gulp-autoprefixer'), 
     nib = require('nib');
  
 
 // Stylus
 gulp.task('stylus', function () {
   gulp.src('src/*.styl') // Исходный файл
-    .pipe(stylus({use: nib(), compress: false})) // Используем nib и сжимаем файл
+    .pipe(stylus({
+      use: nib(), 
+      compress: false
+    })) // Используем nib и сжимаем файл
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    })) // Используем autoprefixer
     .on('error', console.log) // Выводим ошибки в консоль
     .pipe(gulp.dest('build/css/')); // Файл на выходе
 });
